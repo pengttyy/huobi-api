ce02b617f99698c712a1578e6fc319106411f4c3
pengttyy <pengttyy@gmail.com>
2017-12-28 14:27:10 +0800

    feat(project): 初始化项目
    创建项目结构，主要分为两个模块一个是websocket接口封装，一个是rest接口封装

---
+++ huobi-rest/src/main/java/com/pengttyy/rest/HuobiRestApplication.java
@@ -0,0 +1,15 @@
+package com.pengttyy.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author pengttyy pengttyy@gmail.com
+ */
+@SpringBootApplication
+public class HuobiRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuobiRestApplication.class, args);
+    }
+}
---
+++ huobi-websocket/src/test/java/com/pengttyy/websocket/HuobiWebsocketApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pengttyy.websocket;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HuobiWebsocketApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
