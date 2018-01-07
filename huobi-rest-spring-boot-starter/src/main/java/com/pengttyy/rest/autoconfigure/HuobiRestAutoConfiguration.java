fe909b593575dc11274e7623040a4486476ddad3
pengttyy <pengttyy@gmail.com>
2018-01-07 23:57:30 +0800

    feat(config): 相关service的自动化配置

--- huobi-rest-spring-boot-starter/src/main/java/com/pengttyy/rest/autoconfigure/HuobiRestAutoConfiguration.java
+++ huobi-rest-spring-boot-starter/src/main/java/com/pengttyy/rest/autoconfigure/HuobiRestAutoConfiguration.java
@@ -1,8 +1,11 @@
 package com.pengttyy.rest.autoconfigure;
 
 import com.pengttyy.rest.client.HuobiRestTemplate;
-import com.pengttyy.rest.client.IHuobiRestTemplate;
 import com.pengttyy.rest.interceptor.LogInterceptor;
+import com.pengttyy.rest.service.IAccountService;
+import com.pengttyy.rest.service.IorderService;
+import com.pengttyy.rest.service.impl.AccountServiceImpl;
+import com.pengttyy.rest.service.impl.OrderServiceImpl;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -25,13 +28,23 @@ public class HuobiRestAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    public IHuobiRestTemplate restTemplate(RestTemplateBuilder builder, HuobiRestProperties properties, Collection<? extends ClientHttpRequestInterceptor> interceptors) {
+    public HuobiRestTemplate restTemplate(RestTemplateBuilder builder, HuobiRestProperties properties, Collection<? extends ClientHttpRequestInterceptor> interceptors) {
         return builder.setConnectTimeout(60 * 1000).rootUri(properties.getRootUrl()).additionalInterceptors(interceptors).build(HuobiRestTemplate.class);
     }
 
     @Bean
     public ClientHttpRequestInterceptor logInterceptor() {
         return new LogInterceptor();
+    }
+
+    @Bean
+    public IorderService orderService() {
+        return new OrderServiceImpl();
+    }
+
+    @Bean
+    public IAccountService accountService() {
+        return new AccountServiceImpl();
     }
 
 }
